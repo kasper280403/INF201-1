830816c6dd085ac7af0dc6bb43052c77fc88dfdd
henny Brenden <hennybrenden@MacBook-Pro.local>
2025-10-07 09:56:14 +0200

    Added snowflake.py and resource

---
+++ src/main/snowflake.py
@@ -0,0 +1,26 @@
+from turtle import Turtle, Screen
+
+def complete_flake(t, depth):
+    if depth == 0:
+        t.forward(10)          
+    else:                         
+        complete_flake(t, depth-1)  
+        t.left(60)           
+        complete_flake(t, depth-1)  
+        t.right(60)          
+        t.right(60)          
+        complete_flake(t, depth-1) 
+        t.left(60)           
+        complete_flake(t, depth-1)  
+              
+t = Turtle()
+t.speed(0)
+t.penup()
+t.goto(-200, 0)
+t.pendown()
+
+for _ in range(3):
+        complete_flake(t, 3)
+        t.right(120)
+
+Screen().exitonclick()
