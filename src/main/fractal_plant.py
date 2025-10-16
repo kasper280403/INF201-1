758c832a2549613dd4ea5ad51e2b777a4abe94be
kasper280403 <kasper.s.karlsen@gmail.com>
2025-10-16 15:55:25 +0200

    fractal_plant.py added, plant looks like a steelwire-that is a feature

---
+++ src/main/fractal_plant.py
@@ -0,0 +1,60 @@
+from turtle import Turtle, done, Screen
+
+def start():
+    start = "X"
+    angle = 20
+    depth = 5
+    length = 3
+
+    instructions = start
+    for i in range(0, depth):
+        instructions = generate_instructions(instructions)
+
+    draw(instructions, length, angle)
+
+
+def generate_instructions(instructions):
+    new_instructions = ""
+    for char in instructions:
+        if char == "X":
+            new_instructions += "F-[[X]+X]+F[+FX]-X"
+        if char == "F":
+            new_instructions += "FF"
+
+    return new_instructions
+
+def draw(instructions, length, angle):
+    t = Turtle()
+    screen = Screen()
+    screen.tracer(0)
+    t.speed(0)
+    t.color("green")
+    t.penup()
+    t.goto(-200, -250)
+    t.left(90)
+    t.pendown()
+
+    pos = []
+    dir = []
+    for char in instructions:
+        if char == "F":
+            t.forward(length)
+        if char == "[":
+            pos.append(t.position())
+            dir.append(t.heading())
+        if char == "]":
+            t.penup()
+            t.goto(pos[-1][0], pos[-1][1])
+            t.setheading(dir[-1])
+            pos.pop()
+            dir.pop()
+            t.pendown()
+        if char == "+":
+            t.right(angle)
+        if char == "-":
+            t.left(angle)
+
+    screen.update()
+    done()
+
+
