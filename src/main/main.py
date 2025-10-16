2485c1a0d48584c9f9b3e084c58dae215a7fbe9c
kasper280403 <kasper.s.karlsen@gmail.com>
2025-10-16 13:51:25 +0200

    Deleted useless files

--- src/main/main.py
+++ src/main/main.py
@@ -1,13 +1,7 @@
 def main():
-    print("Program is running. Type q/Q to quit.")
+    #print("Program is running. Type q/Q to quit.")
 
     while True:
-
-        user_input = input()
-        if user_input.lower() == "q":
-            print("Program terminated...")
-            break
-
         # Her kan du legge til annen funksjonalitet som skal kjøre kontinuerlig
 
 
